7e5b8dde0ed675d16bc3b9634b28ca4de9f90acd
Abu Taher <writerabutaher@gmail.com>
2023-07-19 23:24:53 +0600

    handling error

--- constants.js
+++ constants.js
@@ -1,3 +1,9 @@
+// * 200 OK: The request was successful, and the server returned the requested resource.
+// * 201 Created: The request was successful, and a new resource was created as a result.
+// * 400 Bad Request: The server could not understand the request due to invalid syntax or malformed request.
+// * 401 Unauthorized: Unauthorized access
+// * 404 Not Found: The requested resource could not be found on the server.
+
 exports.constants = {
   VALIDATION_ERROR: 400,
   UNAUTHORIZED: 401,
--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -4,15 +4,18 @@ const Category = require("../models/categoryModel");
 // get all categories
 const getCategories = asyncHandler(async (req, res) => {
   const categories = await Category.find();
+  if (!categories) {
+    res.status(404);
+    throw new Error("No categories found");
+  }
   res.status(200).json(categories);
 });
 
 // create category
 const createCategory = asyncHandler(async (req, res) => {
   const { name } = req.body;
-  console.log(req.body);
   if (!name) {
-    res.status(400);
+    res.status(404);
     throw new Error("Please enter a category name");
   }
   const category = await Category.create(req.body);
